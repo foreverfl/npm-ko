53251d74d374d8f0f7d350f2c2d351fa644a1385
foreverfl <forever_fl@naver.com>
2024-09-17 18:11:57 +0900

    Translate packages and modules partially

--- src/components/page-footer.js
+++ src/components/page-footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import {Box, Octicon, Avatar, Text} from '@primer/react'
-import {Tooltip} from '@primer/react/next'
-import {PencilIcon} from '@primer/octicons-react'
+import { Box, Octicon, Avatar, Text } from '@primer/react'
+import { Tooltip } from '@primer/react/next'
+import { PencilIcon } from '@primer/octicons-react'
 import Link from './link'
 import usePage from '../hooks/use-page'
 
@@ -22,30 +22,30 @@ const months = [
 
 const format = d => `${months[d.getMonth()]} ${d.getDate()}, ${d.getFullYear()}`
 
-const pluralize = (word, count) => `${word}${count === 1 ? '' : 's'}`
+// const pluralize = (word, count) => `${word}${count === 1 ? '' : 's'}`
 
-const Contributors = ({contributors = [], latestCommit}) => {
+const Contributors = ({ contributors = [], latestCommit }) => {
   if (!contributors.length) {
     return null
   }
 
   return (
     <>
-      <Box sx={{display: 'flex', alignItems: 'center'}}>
-        <Text sx={{mr: 2}}>
-          {contributors.length} {pluralize('contributor', contributors.length)}
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <Text sx={{ mr: 2 }}>
+          총 {contributors.length}명의 기여자
         </Text>
         {contributors.map(login => (
           <Tooltip key={login} text={login}>
-            <Link href={`https://github.com/${login}`} sx={{lineHeight: 'condensedUltra', mr: 2}}>
+            <Link href={`https://github.com/${login}`} sx={{ lineHeight: 'condensedUltra', mr: 2 }}>
               <Avatar src={`https://github.com/${login}.png?size=40`} alt={login} />
             </Link>
           </Tooltip>
         ))}
       </Box>
       {latestCommit ? (
-        <Text sx={{fontSize: 1, mt: 1}}>
-          Last edited by <Link href={`https://github.com/${latestCommit.login}`}>{latestCommit.login}</Link> on{' '}
+        <Text sx={{ fontSize: 1, mt: 1 }}>
+          마지막 수정: <Link href={`https://github.com/${latestCommit.login}`}>{latestCommit.login}</Link>님,{' '}
           <Link href={latestCommit.url}>{format(new Date(latestCommit.date))}</Link>
         </Text>
       ) : null}
@@ -54,7 +54,7 @@ const Contributors = ({contributors = [], latestCommit}) => {
 }
 
 const PageFooter = () => {
-  const {editUrl, latestCommit, contributors = []} = usePage().pageContext
+  const { editUrl, latestCommit, contributors = [] } = usePage().pageContext
 
   if (!editUrl && !contributors.length) {
     return null
@@ -72,11 +72,11 @@ const PageFooter = () => {
         borderColor: 'border.default',
       }}
     >
-      <Box sx={{display: 'grid', gap: 4}}>
+      <Box sx={{ display: 'grid', gap: 4 }}>
         {editUrl ? (
           <Link href={editUrl}>
-            <Octicon icon={PencilIcon} sx={{mr: 2}} />
-            Edit this page on GitHub
+            <Octicon icon={PencilIcon} sx={{ mr: 2 }} />
+            이 페이지를 GitHub에서 편집하기
           </Link>
         ) : null}
         <Contributors contributors={contributors} latestCommit={latestCommit} />
